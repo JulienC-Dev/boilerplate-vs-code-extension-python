7e287a498245ebc089ea052a4a3349acfc095ac3
JulienC-Dev <julien.cormier@yahoo.fr>
2024-07-27 16:00:48 +0200

    wip account flow

--- myapp/main.py
+++ myapp/main.py
@@ -3,24 +3,30 @@ import os
 import argparse
 
 def dump_json(content:dict)->None:
-    path = os.path.join(os.getcwd(), 'account.json')
+    path = os.path.join(os.getcwd(), 'account/account.json')
     with open(path, 'w') as f:
         json.dump(content, f)
     return None
 
 def load_json(path:str)->dict:
-    path = os.path.join(os.getcwd(), 'update_account.json')
+    path = os.path.join(os.getcwd(), 'account/update_account.json')
     with open(path, 'r') as f:
         return json.load(f)
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-u','--update', type=str, help='file update account', required=True)
+    parser.add_argument('-p','--path', type=str, help='Relative path for updating the account', required=True)
     return parser.parse_args()
+
+def is_valid_path(path:str)->bool:
+    return os.path.exists(path)
 
 def main()->int:
   args = get_args()
-  update_account = load_json(args.update)
+  if not is_valid_path(args.path):
+    print('File not found')
+    return 1
+  update_account = load_json(args.path)
   dump_json(update_account)
   return 0
 
